211b78b0abefb3b1a4576846ebe22ef7b27047d5
Calin Marta <calin.mrt@gmail.com>
2019-07-09 16:53:02 +0300

    updates

--- src/main/java/com/sda/vendingMachine/main/App.java
+++ src/main/java/com/sda/vendingMachine/main/App.java
@@ -5,16 +5,15 @@ import com.sda.vendingMachine.models.Coin;
 public class App {
 	public static void main(String[] args) {
 		IVendingMachine atm = VendingMachineImpl.getInstance();
-
 		atm.adminRefillMachine("admin");
 		
-		atm.acceptCoin(Coin.NICKEL);
-		atm.acceptCoin(Coin.NICKEL);
-
-		atm.selectItem(1);
-		System.out.println(atm);
-		atm.cashBalance();
-		System.out.println(atm);
+//		atm.acceptCoin(Coin.NICKEL);
+//		atm.acceptCoin(Coin.NICKEL);
+//
+//		atm.selectItem(1);
+//		System.out.println(atm);
+//		atm.cashBalance();
+//		System.out.println(atm);
 
 	}
 }
--- src/main/java/com/sda/vendingMachine/main/VendingMachineImpl.java
+++ src/main/java/com/sda/vendingMachine/main/VendingMachineImpl.java
@@ -22,6 +22,8 @@ public class VendingMachineImpl implements IVendingMachine {
 		}
 		return instance;
 	}
+	
+	
 
 	@Override
 	public void acceptCoin(Coin coin) {
